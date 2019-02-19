5b9176844443cd453e45f601e0297e0d78591dae
Antony.Glim <Antony.Glim@gmail.com>
2019-02-19 15:20:11 +0300

    ReadFromConsol change

--- src/main/java/IO/CopyFilesSymbolBySymbol.java
+++ src/main/java/IO/CopyFilesSymbolBySymbol.java
@@ -1,6 +1,5 @@
 package IO;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -14,16 +13,16 @@ public class CopyFilesSymbolBySymbol {
 
     public static void main(String[] args) throws IOException {
 
-        FileReader reader = new FileReader("c:/data.txt");
-        FileWriter writer = new FileWriter("c:/result.txt");
+        FileReader fileReader = new FileReader("c:/data.txt");
+        FileWriter fileWriter = new FileWriter("c:/result.txt");
 
         int symbol;
-        while (reader.ready()){ //пока есть непрочитанные байты в потоке ввода
-            symbol = reader.read(); //читаем один символ (char будет расширен до int)
-            writer.write(symbol); //пишем один символ (int будет обрезан/сужен до char)
+        while (fileReader.ready()){ //пока есть непрочитанные байты в потоке ввода
+            symbol = fileReader.read(); //читаем один символ (char будет расширен до int)
+            fileWriter.write(symbol); //пишем один символ (int будет обрезан/сужен до char)
         }
 
-        reader.close();
-        writer.close();
+        fileReader.close();
+        fileWriter.close();
     }
 }
