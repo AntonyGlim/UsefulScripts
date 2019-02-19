fc9be3737e89b6d7efd4eb714004a88ce7a904c8
Antony.Glim <Antony.Glim@gmail.com>
2019-02-19 17:28:54 +0300

    CopyFilesSymbolWithBuffer

---
+++ src/main/java/IO/CopyFilesSymbolWithBuffer.java
@@ -0,0 +1,24 @@
+package IO;
+
+import java.io.*;
+
+public class CopyFilesSymbolWithBuffer {
+
+    public static void main(String[] args) throws IOException {
+        FileReader fileReader = new FileReader("c:/data.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        FileWriter fileWriter = new FileWriter("c:/result.txt");
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+        while (bufferedReader.ready()){
+            bufferedWriter.write((char) bufferedReader.read());
+        }
+
+        bufferedWriter.close();
+        fileWriter.close();
+
+        bufferedReader.close();
+        fileReader.close();
+    }
+}
