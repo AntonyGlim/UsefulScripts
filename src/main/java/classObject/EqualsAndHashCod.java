2c37c1cb2c9d354c932a3252c02593c820f58e27
Antony.Glim <Antony.Glim@gmail.com>
2019-03-04 21:50:18 +0300

    EqualsAndHashCod add

--- src/main/java/classObject/EqualsAndHashCod.java
+++ src/main/java/classObject/EqualsAndHashCod.java
@@ -1,4 +1,13 @@
 package classObject;
 
+/**
+ * https://javarush.ru/groups/posts/1340-peregruzka-metodov-equals-i-hashcode-v-java
+ * https://javarush.ru/groups/posts/1989-kontraktih-equals-i-hashcode-ili-kak-ono-vsje-tam
+ * В статьях описываются правила переопределения
+ * методов:
+ * public boolean equals(Object o) и
+ * public int hashCode()
+ */
 public class EqualsAndHashCod {
+    
 }
