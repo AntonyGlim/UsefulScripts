cf785f6b0663f95ec403b3a6c98e9893e976d2e1
Antony.Glim@gmail.com <Antony.Glim@gmail.com>
2019-05-24 12:27:12 +0300

    WriteFileIntoZIP add

--- src/main/java/zip/WriteFileIntoZIP.java
+++ src/main/java/zip/WriteFileIntoZIP.java
@@ -1,4 +1,30 @@
 package zip;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * Добавление файла в архив
+ */
 public class WriteFileIntoZIP {
+    public static void main(String[] args) throws IOException {
+        // создаем архив
+        FileOutputStream zipFile = new FileOutputStream("D:/1/1/test.zip");
+        ZipOutputStream zip = new ZipOutputStream(zipFile);
+
+        //кладем в него ZipEntry – «архивный объект»
+        //это пока просто имя?
+        zip.putNextEntry(new ZipEntry("3.jpg"));
+
+        //копируем файл "D:/1/1/3.jpg" в энтри "3.jpg" в архиве
+        File file = new File("D:/1/3.jpg");
+        Files.copy(file.toPath(), zip);
+
+        //закрываем поток
+        zip.close();
+    }
 }
